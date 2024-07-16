aea5ff3b3cba91b24bf1df81a2b927f01ef8ff0b
shivam8112005 <shivamshukla8112005@gmail.com>
2024-07-16 19:13:33 +0530

    added customermenu class and its content

--- Passenger.java
+++ Passenger.java
@@ -1,22 +1,26 @@
+import java.util.LinkedList;
+
 class Passenger extends User {
     private static int idCounter = 1;
+    LinkedList<Passenger> passengers=new LinkedList<>();
 
-    public Passenger(String name, String phoneNumber, Ticket ticket, String dateOfBirth, DiscountPass discountPass) {
-        super(idCounter++, name, phoneNumber, ticket, dateOfBirth, discountPass);
-    }
 
-    public Passenger(Customer customer) {
-        super(idCounter++, customer.getName(), customer.getPhoneNumber(), null, customer.getDateOfBirth(), null);
-        // Copy other relevant data if necessary
-    }
+    // public Passenger(String name, String phoneNumber, Ticket ticket, String dateOfBirth, DiscountPass discountPass) {
+    //     super(idCounter++, name, phoneNumber, ticket, dateOfBirth, discountPass);
+    // }
 
-    @Override
-    public String toString() {
-        return "Passenger{" +
-                "id=" + getId() +
-                ", name='" + getName() + '\'' +
-                ", phoneNumber='" + getPhoneNumber() + '\'' +
-                ", dateOfBirth='" + getDateOfBirth() + '\'' +
-                '}';
-    }
+    // public Passenger(Customer customer) {
+    //     super(idCounter++, customer.getName(), customer.getPhoneNumber(), null, customer.getDateOfBirth(), null);
+    //     // Copy other relevant data if necessary
+    // }
+
+    // @Override
+    // public String toString() {
+    //     return "Passenger{" +
+    //             "id=" + getId() +
+    //             ", name='" + getName() + '\'' +
+    //             ", phoneNumber='" + getPhoneNumber() + '\'' +
+    //             ", dateOfBirth='" + getDateOfBirth() + '\'' +
+    //             '}';
+    // }
 }
